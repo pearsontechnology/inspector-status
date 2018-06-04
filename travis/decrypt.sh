6da68f05c48f78fa3e4e7ccdac006ea9251a86b0
Bonamy Klu <bonamyklu@gmail.com>
2018-06-04 18:25:43 +0100

    Update creds

--- travis/decrypt.sh
+++ travis/decrypt.sh
@@ -6,4 +6,4 @@ set -e
 set -x
 # Decrypt AWS Credentials
 mkdir ~/.aws
-openssl aes-256-cbc -K $encrypted_eb65b30e22c0_key -iv $encrypted_eb65b30e22c0_iv -in credentials.enc -out credentials -d
+openssl aes-256-cbc -K $encrypted_ed64d377ad9e_key -iv $encrypted_ed64d377ad9e_iv -in credentials.enc -out credentials -d
