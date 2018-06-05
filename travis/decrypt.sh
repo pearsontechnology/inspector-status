b08a37d38d87e59aaf83e23546af1d5d24f2190c
sacreman <sacreman@gmail.com>
2018-06-05 20:25:20 +0700

    rotate travis keys

--- travis/decrypt.sh
+++ travis/decrypt.sh
@@ -6,4 +6,4 @@ set -e
 set -x
 # Decrypt AWS Credentials
 mkdir ~/.aws
-openssl aes-256-cbc -K $encrypted_ed64d377ad9e_key -iv $encrypted_ed64d377ad9e_iv -in credentials.enc -out credentials -d
+openssl aes-256-cbc -K $encrypted_b3714d782fc6_key -iv $encrypted_b3714d782fc6_iv -in credentials.enc -out credentials -d
